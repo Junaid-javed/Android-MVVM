8e8585df5e68bbb98e0c9c7106f8f07bdcc05173
Junaid <junaidjaved1618@gmail.com>
2023-12-25 15:24:37 +0500

    initial commit

--- app/src/main/java/com/example/mvvmdemo/MainFragment.kt
+++ app/src/main/java/com/example/mvvmdemo/MainFragment.kt
@@ -14,6 +14,7 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
